8ae25c64dc0f8b27176f8213c6d96fa44893bab7
guddnboy <hung3018@naver.com>
2024-06-10 14:46:23 +0900

    add(vending-machine-container)

---
+++ src/VendingMachineContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./VendingMachine.css";
+
+const VendingMachineContainer = () => {
+  return (
+    <div className="vending-machine-container">
+        <h1>VendingMachine</h1>
+        <div className="vending-machine">
+          <p>이곳에 벤딩머신 둘 예정</p>
+        </div>
+    </div>
+  );
+};
+
+export default VendingMachineContainer;
