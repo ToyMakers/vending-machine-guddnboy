1e4ecce7e548a01fa0efcdcca0308b17a5874c8e
guddnboy <hung3018@naver.com>
2024-06-11 17:27:31 +0900

    refact(Money js 추가)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import VendingMachineContainer from './components/containers/VendingMachineContainer';
 import Machine from './components/Machine';
+import Money from '.'
 
 function App() {
   return (
     <div className='App'>
       <VendingMachineContainer />
       <Machine />
+      <Money />
+
       </div>
   );
 }
