e86948f041387edebe0cef4cad374741cbd406a0
guddnboy <hung3018@naver.com>
2024-06-15 16:22:30 +0900

    style(hover effect)

--- src/components/Machine.js
+++ src/components/Machine.js
@@ -1,19 +1,46 @@
 import React from "react";
 import "../css/VendingMachine.css";
+import "../css/drinks.css";
 import MoneyContainer from "./containers/MoneyContainer";
+import beeaImg from "../img/beer.png";
+import bluehawia from "../img/bluehawai.png";
+import bubbletea from "../img/bubbletea.png";
+import coke from "../img/coke.png";
+import fanta from "../img/fanta.png";
+import lemonade from "../img/lemonade.png";
+import orangejuice from "../img/orangejuice.png";
+import vita500 from "../img/vita500.png";
+
 
 const Machine = () => {
     return(
         <div className="vending-machine">
           <div className="drink-list">
-            <button className="drinks">1</button>
-            <button className="drinks">2</button>
-            <button className="drinks">3</button>
-            <button className="drinks">4</button>
-            <button className="drinks">5</button>
-            <button className="drinks">6</button>
-            <button className="drinks">7</button>
-            <button className="drinks">8</button>
+            <button className="drinks">
+            <img src={beeaImg} alt="맥주" onclick="" ></img>
+            </button>
+            <button className="drinks">
+            <img src={bluehawia} alt="블루하와이" onclick="" ></img>
+            </button>
+
+            <button className="drinks">
+            <img src={bubbletea} alt="버블티" onclick="" ></img>
+            </button>
+            <button className="drinks">
+            <img src={coke} alt="콜라" onclick="" ></img>
+            </button>
+            <button className="drinks">
+            <img src={fanta} alt="환타" onclick="" ></img>
+            </button>
+            <button className="drinks">
+            <img src={lemonade} alt="레모네이드" onclick="" ></img>
+            </button>
+            <button className="drinks">
+            <img src={orangejuice} alt="오렌지주스" onclick="" ></img>
+            </button>
+            <button className="drinks">
+            <img src={vita500} alt="비타500" onclick="" ></img>
+            </button>
           </div>
           <div className="money-container">
             <MoneyContainer />
