73a3ee25fbb54f405eefb8e421bacd5df49e47e9
guddnboy <hung3018@naver.com>
2024-06-19 21:24:52 +0900

    style(요소 배치 수정)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import VendingMachineContainer from './components/containers/VendingMachineContainer';
-import MoneyContainer from './components/containers/MoneyContainer';
+import WalletContainer from './components/containers/WalletContainer';
 
 function App() {
   return (
     <div className='App'>
-      <div>
+      <div className='containers'>
         <VendingMachineContainer />
-        <MoneyContainer />
+        <WalletContainer />
       </div>
     </div>
   );
