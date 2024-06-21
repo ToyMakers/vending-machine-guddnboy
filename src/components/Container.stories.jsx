9b7fef1df139897838517f6ae483b1a2aaf0d6a0
guddnboy <hung3018@naver.com>
2024-06-21 15:18:02 +0900

    chore(storybook 설치)

---
+++ src/components/Container.stories.jsx
@@ -0,0 +1,9 @@
+import Container from './Container';
+
+const meta = {
+  component: Container,
+};
+
+export default meta;
+
+export const Default = {};
---
+++ src/components/DrinkList.stories.jsx
@@ -0,0 +1,11 @@
+import DrinkList from './DrinkList';
+
+const meta = {
+  component: DrinkList,
+};
+
+export default meta;
+
+export const Default = {
+  args: {}
+};
