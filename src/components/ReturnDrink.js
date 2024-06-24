1687647f0084a6f8f1667503bac0d60137fc7be0
guddnboy <hung3018@naver.com>
2024-06-24 15:58:21 +0900

    feat(구매한 음료 반환)

--- src/components/ReturnDrink.js
+++ src/components/ReturnDrink.js
@@ -1,17 +1,34 @@
-/* eslint-disable array-callback-return */
-import React, { useState } from "react";
+import React from "react";
 import "../css/VendingMachine.css";
 import "../css/drinks.css";
 import insert_coin from "../img/insert.png";
+import bearImg from "../img/drinks/beer.png";
+import bluehawai from "../img/drinks/bluehawai.png";
+import bubbletea from "../img/drinks/bubbletea.png";
+import coke from "../img/drinks/coke.png";
+import fanta from "../img/drinks/fanta.png";
+import lemonade from "../img/drinks/lemonade.png";
+import orangejuice from "../img/drinks/orangejuice.png";
+import vita500 from "../img/drinks/vita500.png";
 
 const ReturnDrink = ({
     insert,
     insertMoney,
     returnMoney,
-    returnDrinks,
-    selectedDrink,
-    onClickResetDrinks,
+    getMyDrinks,
+    myDrinks,
 }) => {
+    const drinkImages = {
+        맥주: bearImg,
+        블루하와이: bluehawai,
+        버블티: bubbletea,
+        콜라: coke,
+        환타: fanta,
+        레몬에이드: lemonade,
+        오렌지주스: orangejuice,
+        비타500: vita500,
+    };
+
     return (
         <div>
             <div className="money-display">
@@ -35,10 +52,18 @@ const ReturnDrink = ({
             </div>
             <div className="drink-out-container">
                 <div className="drink-out">
-                    <button></button>
+                    <button onClick={getMyDrinks}></button>
                 </div>
-                <div className="bought-drinks">
-                    <li>{returnDrinks}</li>
+                <div>
+                    {myDrinks.map((drink, name) => (
+                        <img
+                            key={name}
+                            src={drinkImages[drink.name]}
+                            alt={drink.name}
+                            width="45px"
+                            height="45px"
+                        />
+                    ))}
                 </div>
             </div>
         </div>
