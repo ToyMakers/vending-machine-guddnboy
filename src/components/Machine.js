520335c24b99641369004e5cba8ad396ad6566be
guddnboy <hung3018@naver.com>
2024-06-27 21:06:11 +0900

    refact(불필요한 div 삭제)

--- src/components/Machine.js
+++ src/components/Machine.js
@@ -5,14 +5,12 @@ import DrinkList from "./DrinkList";
 
 const Machine = () => {
     return (
-        <div className="vending-machine-container">
-            <div className="vending-machine">
-                <div>
-                    <h3>나만의 자판기</h3>
-                </div>
-                <DrinkList />
-                <ReturnDrink />
+        <div className="vending-machine">
+            <div>
+                <h3>나만의 자판기</h3>
             </div>
+            <DrinkList />
+            <ReturnDrink />
         </div>
     );
 };
