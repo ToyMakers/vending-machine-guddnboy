40877a1cf18a85d668aa625e6964e28d59e04fc5
guddnboy <hung3018@naver.com>
2024-07-01 16:19:18 +0900

    delete(insert 기능 삭제)

--- src/components/ReturnDrink.js
+++ src/components/ReturnDrink.js
@@ -1,21 +1,21 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "../css/VendingMachine.css";
 import "../css/drinks.css";
 import insert_coin from "../img/insert.png";
 import useStore from "./store.js";
 
 const ReturnDrink = () => {
-    const { insertMoney, insert, returnMoney, getMyDrinks } = useStore(
+    const { insertMoney, returnMoney, getMyDrinks } = useStore(
         (state) => state
     );
 
     return (
-        <Fragment>
+        <div className="return-element">
             <div className="money-display">
                 <div className="insert-money">
                     <div className="display-insert">{insertMoney}</div>
                     <div className="insert-return">
-                        <button onClick={insert}>
+                        <button>
                             <img
                                 src={insert_coin}
                                 alt="insert"
@@ -35,7 +35,7 @@ const ReturnDrink = () => {
                     <button onClick={getMyDrinks}></button>
                 </div>
             </div>
-        </Fragment>
+        </div>
     );
 };
 
